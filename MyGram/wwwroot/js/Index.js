05f0ce74123a9b385fab5a02194748109b4e344b
Pablo Gamez Jr <>
2021-04-02 13:44:29 -0600

    Module 1 JAVA

---
+++ MyGram/wwwroot/js/Index.js
@@ -0,0 +1,21 @@
+﻿var btn = document.createElement("Button");
+btn.innerHTML = "Log In";
+document.body.appendChild(btn);
+var btn = document.createElement("Button");
+btn.innerHTML = "Sing Up";
+document.body.appendChild(btn);
+
+
+var SecondSpan = document.getElementById("SecondSpan");
+var ThirdSpan = document.getElementById("ThirdSpan");
+var FourhtSpan = document.getElementById("FourthSpan");
+
+
+//var newSpan = document.createElement("span");
+//newSpan.setAttribute("id", "SecondSpan");
+
+//newSpan.innerHTML = "This is a text";
+//var newText = document.createTextNode("This is a text");
+//newSpan.appendChild(newText);
+//div[0].insertBefore(newSpan, firstSpan);
+
