f1d8ed283018cb3cedc43461434f4a0cd16f3f10
Pablo Gamez Jr <>
2021-04-17 02:47:36 -0600

    Module 7

--- MyGram/wwwroot/js/Index.js
+++ MyGram/wwwroot/js/Index.js
@@ -1,51 +1,254 @@
-﻿
+﻿var span = document.createElement("span");
+span.innerHTML = "My Gram  ";
 
-var container = document.getElementById("MainContainer");
-var span = document.createElement("span");
-span.innerHTML = "My Gram &nbsp;&nbsp;";
+var div = document.getElementById("MainContainer");
 
-container.appendChild(span);
+div.appendChild(span);
 
-var btn = document.createElement("Button");
-btn.innerHTML = "Log In";
-document.body.appendChild(btn);
+var loginButton =
+    document.createElement("button");
+loginButton.innerHTML = "Log In";
+div.appendChild(loginButton);
 
-container.appendChild(btn);
-document.write('&nbsp;&nbsp;&nbsp;');
+var signUpButton =
+    document.createElement("button");
+signUpButton.innerHTML = "Sign Up";
+div.appendChild(signUpButton);
 
-var btn = document.createElement("Button");
-btn.innerHTML = "Sing Up";
-document.body.appendChild(btn);
+var lineBreak = document.createElement("br");
+div.appendChild(lineBreak);
 
-container.appendChild(btn);
-
-document.write('</br></br>');
-
-var container = document.getElementById("MainContainer");
-var newSpan = document.createElement("span");
-newSpan.innerHTML = "Image1  Image2  Image3  Image4 Image5 </br></br>";
-
-container.appendChild(newSpan);
-
-
-document.write('</br>');
-
-
-var container = document.getElementById("MainContainer");
-var newSpan2 = document.createElement("span");
-newSpan2.innerHTML = "About Blog Jobs Help";
-
-container.appendChild(newSpan2);
-document.write('</br>');
-
-var container = document.getElementById("MainContainer");
-var newSpan3 = document.createElement("span");
-newSpan3.innerHTML = "English 2021 MyGram";
-
-container.appendChild(newSpan3);
+var lineBreak = document.createElement("br");
+div.appendChild(lineBreak);
 
 
 
+var image1Span = document.createElement("span");
+
+image1Span.innerHTML = "Image 1  ";
+
+div.appendChild(image1Span);
+
+
+var image2Span = document.createElement("span");
+
+image2Span.innerHTML = "  Image 2  ";
+
+div.appendChild(image2Span);
+
+var image3Span = document.createElement("span");
+
+image3Span.innerHTML = "  Image 3  ";
+
+div.appendChild(image3Span);
+
+var image4Span = document.createElement("span");
+
+image4Span.innerHTML = "  Image 4  ";
+
+div.appendChild(image4Span);
+
+var image5Span = document.createElement("span");
+
+image5Span.innerHTML = "  Image 5  ";
+
+div.appendChild(image5Span);
+
+var lineBreak = document.createElement("br");
+div.appendChild(lineBreak);
+
+var image6Span = document.createElement("span");
+
+image6Span.innerHTML = "Image  6 ";
+
+div.appendChild(image6Span);
+
+
+var image7Span = document.createElement("span");
+
+image7Span.innerHTML = "  Image 7  ";
+
+div.appendChild(image7Span);
+
+var image8Span = document.createElement("span");
+
+image8Span.innerHTML = "  Image 8  ";
+
+div.appendChild(image8Span);
+
+var image9Span = document.createElement("span");
+
+image9Span.innerHTML = "  Image 9  ";
+
+div.appendChild(image9Span);
+
+var image10Span = document.createElement("span");
+
+image10Span.innerHTML = "  Image 10  ";
+
+div.appendChild(image10Span);
+
+var lineBreak = document.createElement("br");
+div.appendChild(lineBreak);
+
+var image11Span = document.createElement("span");
+
+image11Span.innerHTML = "Image  11 ";
+
+div.appendChild(image11Span);
+
+
+var image12Span = document.createElement("span");
+
+image12Span.innerHTML = "  Image 12  ";
+
+div.appendChild(image12Span);
+
+var image13Span = document.createElement("span");
+
+image13Span.innerHTML = "  Image 13  ";
+
+div.appendChild(image13Span);
+
+var image14Span = document.createElement("span");
+
+image14Span.innerHTML = "  Image 14  ";
+
+div.appendChild(image14Span);
+
+var image15Span = document.createElement("span");
+
+image15Span.innerHTML = "  Image 15  ";
+
+div.appendChild(image15Span);
+
+var lineBreak = document.createElement("br");
+div.appendChild(lineBreak);
+
+var image16Span = document.createElement("span");
+
+image16Span.innerHTML = "Image  16 ";
+
+div.appendChild(image16Span);
+
+
+var image17Span = document.createElement("span");
+
+image17Span.innerHTML = "  Image 17  ";
+
+div.appendChild(image17Span);
+
+var image18Span = document.createElement("span");
+
+image18Span.innerHTML = "  Image 18  ";
+
+div.appendChild(image18Span);
+
+var image19Span = document.createElement("span");
+
+image19Span.innerHTML = "  Image 19  ";
+
+div.appendChild(image19Span);
+
+var image20Span = document.createElement("span");
+
+image20Span.innerHTML = "  Image 20  ";
+
+div.appendChild(image20Span);
+
+var lineBreak = document.createElement("br");
+div.appendChild(lineBreak);
+
+var image21Span = document.createElement("span");
+
+image21Span.innerHTML = "Image  21 ";
+
+div.appendChild(image21Span);
+
+
+var image22Span = document.createElement("span");
+
+image22Span.innerHTML = "  Image 22  ";
+
+div.appendChild(image22Span);
+
+var image23Span = document.createElement("span");
+
+image23Span.innerHTML = "  Image 23  ";
+
+div.appendChild(image23Span);
+
+var image24Span = document.createElement("span");
+
+image24Span.innerHTML = "  Image 24  ";
+
+div.appendChild(image24Span);
+
+var image25Span = document.createElement("span");
+
+image25Span.innerHTML = "  Image 25  ";
+
+div.appendChild(image25Span);
+
+var lineBreak = document.createElement("br");
+div.appendChild(lineBreak);
+
+var lineBreak = document.createElement("br");
+div.appendChild(lineBreak);
+
+var aboutSpan = document.createElement("span");
+
+aboutSpan.innerHTML = "About  ";
+
+div.appendChild(aboutSpan);
+
+
+var blogSpan = document.createElement("span");
+
+blogSpan.innerHTML = "  Blog  ";
+
+div.appendChild(blogSpan);
+
+var jobsSpan = document.createElement("span");
+
+jobsSpan.innerHTML = "  Jobs  ";
+
+div.appendChild(jobsSpan);
+
+var helpSpan = document.createElement("span");
+
+helpSpan.innerHTML = "  Help  ";
+
+div.appendChild(helpSpan);
+
+
+var lineBreak = document.createElement("br");
+div.appendChild(lineBreak);
+
+var englishSpan = document.createElement("span");
+
+englishSpan.innerHTML = "English  ";
+
+div.appendChild(englishSpan);
+
+
+var yearSpan = document.createElement("span");
+
+yearSpan.innerHTML = "  2021  ";
+
+div.appendChild(yearSpan);
+
+var mySpan = document.createElement("span");
+
+mySpan.innerHTML = "  My  ";
+
+div.appendChild(mySpan);
+
+var gramSpan = document.createElement("span");
+
+gramSpan.innerHTML = "  Gram  ";
+
+div.appendChild(gramSpan);
 
 
 
@@ -109,31 +312,18 @@ container.appendChild(newSpan3);
 //console.log(e);
 //console.log(h);
 
-"use strict";
+//"use strict";
 
-document.write('</br>');
-var d = new Date(); 
-
-
-document.write((d.getMonth() + 1).toString().padStart(2, "0") + "/" +
-d.getMonth().toString().padStart(2, "0") + "/" +
-d.getFullYear());
-
-document.write('</br>');
-
-document.write(d.toLocaleString("ja-JP"));
+//document.write('</br>');
+//var d = new Date(); 
 
 
+//document.write((d.getMonth() + 1).toString().padStart(2, "0") + "/" +
+//d.getMonth().toString().padStart(2, "0") + "/" +
+//d.getFullYear());
 
+//document.write('</br>');
 
-
-
-
-
-
-
-
-
-
+//document.write(d.toLocaleString("ja-JP"));
 
 
