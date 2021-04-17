288b932bad926f4a57dade446c61da85fb478846
Pablo Gamez Jr <>
2021-04-17 04:00:41 -0600

    Module 7

--- MyGram/wwwroot/js/Index.js
+++ MyGram/wwwroot/js/Index.js
@@ -21,174 +21,39 @@ div.appendChild(lineBreak);
 var lineBreak = document.createElement("br");
 div.appendChild(lineBreak);
 
+var table = document.createElement("table");
+var tbody = document.createElement("tbody");
+var trow = document.createElement("tr");;
+var td = null;
+var imageSpan = null;
+var counter = 0;
 
 
-var image1Span = document.createElement("span");
+for (var i = 1; i <= 25; i++) {
 
-image1Span.innerHTML = "Image 1  ";
+    if (counter == 5) {
 
-div.appendChild(image1Span);
+        tbody.appendChild(trow);
+        trow = document.createElement("tr");
+        counter = 0;
+    }
+        
+        td = document.createElement("td");
+        imageSpan = document.createElement("span");
+        imageSpan.innerHTML = "Image  " + i;
+   
 
+    td.appendChild(imageSpan);
 
-var image2Span = document.createElement("span");
+    trow.appendChild(td);
 
-image2Span.innerHTML = "  Image 2  ";
+    counter++;
+}
+tbody.appendChild(trow);
 
-div.appendChild(image2Span);
+table.appendChild(tbody);
 
-var image3Span = document.createElement("span");
-
-image3Span.innerHTML = "  Image 3  ";
-
-div.appendChild(image3Span);
-
-var image4Span = document.createElement("span");
-
-image4Span.innerHTML = "  Image 4  ";
-
-div.appendChild(image4Span);
-
-var image5Span = document.createElement("span");
-
-image5Span.innerHTML = "  Image 5  ";
-
-div.appendChild(image5Span);
-
-var lineBreak = document.createElement("br");
-div.appendChild(lineBreak);
-
-var image6Span = document.createElement("span");
-
-image6Span.innerHTML = "Image  6 ";
-
-div.appendChild(image6Span);
-
-
-var image7Span = document.createElement("span");
-
-image7Span.innerHTML = "  Image 7  ";
-
-div.appendChild(image7Span);
-
-var image8Span = document.createElement("span");
-
-image8Span.innerHTML = "  Image 8  ";
-
-div.appendChild(image8Span);
-
-var image9Span = document.createElement("span");
-
-image9Span.innerHTML = "  Image 9  ";
-
-div.appendChild(image9Span);
-
-var image10Span = document.createElement("span");
-
-image10Span.innerHTML = "  Image 10  ";
-
-div.appendChild(image10Span);
-
-var lineBreak = document.createElement("br");
-div.appendChild(lineBreak);
-
-var image11Span = document.createElement("span");
-
-image11Span.innerHTML = "Image  11 ";
-
-div.appendChild(image11Span);
-
-
-var image12Span = document.createElement("span");
-
-image12Span.innerHTML = "  Image 12  ";
-
-div.appendChild(image12Span);
-
-var image13Span = document.createElement("span");
-
-image13Span.innerHTML = "  Image 13  ";
-
-div.appendChild(image13Span);
-
-var image14Span = document.createElement("span");
-
-image14Span.innerHTML = "  Image 14  ";
-
-div.appendChild(image14Span);
-
-var image15Span = document.createElement("span");
-
-image15Span.innerHTML = "  Image 15  ";
-
-div.appendChild(image15Span);
-
-var lineBreak = document.createElement("br");
-div.appendChild(lineBreak);
-
-var image16Span = document.createElement("span");
-
-image16Span.innerHTML = "Image  16 ";
-
-div.appendChild(image16Span);
-
-
-var image17Span = document.createElement("span");
-
-image17Span.innerHTML = "  Image 17  ";
-
-div.appendChild(image17Span);
-
-var image18Span = document.createElement("span");
-
-image18Span.innerHTML = "  Image 18  ";
-
-div.appendChild(image18Span);
-
-var image19Span = document.createElement("span");
-
-image19Span.innerHTML = "  Image 19  ";
-
-div.appendChild(image19Span);
-
-var image20Span = document.createElement("span");
-
-image20Span.innerHTML = "  Image 20  ";
-
-div.appendChild(image20Span);
-
-var lineBreak = document.createElement("br");
-div.appendChild(lineBreak);
-
-var image21Span = document.createElement("span");
-
-image21Span.innerHTML = "Image  21 ";
-
-div.appendChild(image21Span);
-
-
-var image22Span = document.createElement("span");
-
-image22Span.innerHTML = "  Image 22  ";
-
-div.appendChild(image22Span);
-
-var image23Span = document.createElement("span");
-
-image23Span.innerHTML = "  Image 23  ";
-
-div.appendChild(image23Span);
-
-var image24Span = document.createElement("span");
-
-image24Span.innerHTML = "  Image 24  ";
-
-div.appendChild(image24Span);
-
-var image25Span = document.createElement("span");
-
-image25Span.innerHTML = "  Image 25  ";
-
-div.appendChild(image25Span);
+div.appendChild(table);
 
 var lineBreak = document.createElement("br");
 div.appendChild(lineBreak);
